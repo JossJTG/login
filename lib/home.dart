7b53120c816a7f9902184f4b9e1a47688021c0ce
Josias Toribio <josias10.jtg@gmail.com>
2023-07-03 17:03:09 -0500

    Login listo

---
+++ lib/home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const HomePage());
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  static const appTitle = "Drawer Demo";
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: const Center(
+        child: Text('Mi página de acceso')),
+      drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue, 
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: (){Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: (){Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,70 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+import 'forget_password.dart';
+
+class RegisterPage extends StatelessWidget{
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void _registerUser(BuildContext) async {
+    try{
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text, password: passwordController.text);
+    }catch(e){
+      print("Error: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Registrar'),
+      ),
+      body: 
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Correo',
+                ),
+              ),
+
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Contraseña',
+                ),
+                obscureText: true,
+              ),
+              
+              const SizedBox(height: 16.0,),
+              ElevatedButton(
+                onPressed: () => _registerUser(context),
+                child: const Text('Registrar'),
+              ),
+
+              const SizedBox(height: 16.0,),
+              TextButton(onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            }, 
+            child: const Text('Already have an account? Login here')
+            ),
+            const SizedBox(height: 16.0,),
+              TextButton(onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => ForgetPasswordPage()),
+              );
+            }, 
+            child: const Text('Forgot your Password'),
+            ),  
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
